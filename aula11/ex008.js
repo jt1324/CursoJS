55076f83b074943c8206fd8a16cd5960af626bfa
Jean <jeantoneli@gmail.com>
2025-06-02 23:14:12 +0100

    using if and else

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+const vel = 59.5
+console.log(`The speed is ${vel} Km/h`);
+if( vel >60) {
+  console.log('You were got speeding, you got a fine');
+} else {
+  console.log('Always use the belt while driving');
+};
+
