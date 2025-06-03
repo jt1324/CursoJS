636a9ada81f02efc699c1f13094f1aef0ca13add
Jean <jeantoneli@gmail.com>
2025-06-03 22:01:25 +0100

    Nested if statements

---
+++ aula12/ex012.js
@@ -0,0 +1,10 @@
+const age = 65;
+if (age < 16) {
+  console.log('You cannot vote')
+} else if (age >= 16 && age <18) {
+   console.log('Vote is optional')
+} else if (age >= 65) {
+    console.log('Vote is opitional for seniors')
+} else {
+   console.log('Vote is mandatory') 
+}
