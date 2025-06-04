037d6df96d9ccff6d57200404ffaae8646b208c7
Jean <jeantoneli@gmail.com>
2025-06-04 23:25:04 +0100

    Training LOOPS - While | Do While

---
+++ ex016/loop-test.js
@@ -0,0 +1,19 @@
+//WHILE - LOOP SIMPLE - TEST AT THE BEGINNING
+let cont = 1
+while (cont <= 5) {
+  console.log(`step ${cont}`)
+  cont++ //same as cont = cont + 1
+}
+
+
+//DO WHILE - LOOP SIMPLE - TEST AT THE END
+let count = 1
+do {
+  console.log(`step ${count}`)
+  count++ //same as count = count + 1
+} while (count <= 5)
+
+
+
+
+ 
