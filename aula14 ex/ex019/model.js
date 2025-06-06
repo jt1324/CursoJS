ed04160e1c8ee90eb0e51f48266a56b7cbb13bb8
Jean <jeantoneli@gmail.com>
2025-06-06 22:53:35 +0100

    starting exercise 19 by my own

--- aula14 ex/ex019/model.js
+++ aula14 ex/ex019/model.js
@@ -1,1 +1,8 @@
-alert('Hello')
+function generate() {
+  let add = document.getElementById('addtxt')
+  let res = document.getElementById('res')
+  if (add.value.length ==0 )  {
+    alert('Please add a number')
+  } else 
+
+}
