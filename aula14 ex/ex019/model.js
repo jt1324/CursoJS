d590dd386abcaa0daa41fe010e6ba5a96a5a8ada
Jean <jeantoneli@gmail.com>
2025-06-06 23:20:36 +0100

    finalised exercise

--- aula14 ex/ex019/model.js
+++ aula14 ex/ex019/model.js
@@ -1,8 +1,19 @@
 function generate() {
-  let add = document.getElementById('addtxt')
-  let res = document.getElementById('res')
-  if (add.value.length ==0 )  {
+  let num = document.getElementById('addtxt')
+  let res = document.getElementById('seltab')
+  if(num.value.length == 0) {
     alert('Please add a number')
-  } else 
+  } else { 
+    let n = Number(num.value)
+    let c = 1
+    res.innerHTML = '' //clean previous result
+    while (c <=10) {
+      let item = document.createElement('option') //add <option> in HTML from JS
+      item.text = `${n} x ${c} = ${n*c}` 
+      res.appendChild(item) //add items inside <select>, like <options>
+      c++
+    }
+
+  }
 
 }
