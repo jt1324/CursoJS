ee47c1cd9bcccbed7a994da344c8f781c571092c
Jean <jeantoneli@gmail.com>
2025-06-06 22:34:32 +0100

    exercise class 14 finished

--- aula14 ex/ex018/model.js
+++ aula14 ex/ex018/model.js
@@ -4,16 +4,28 @@ function count() {
   let end = document.getElementById('txte')
   let fac = document.getElementById('txtf')
   if (sta.value.length == 0 || end.value.length == 0 || fac.value.length ==0) {
+    res.innerHTML = 'Impossible to count!'
     alert('[ERROR] Missing information')
   } else {
-    res.innerHTML = 'Counting: '
+    res.innerHTML = 'Counting: <br>'
     let s = Number(sta.value)
     let e = Number(end.value)
     let f = Number(fac.value)
-    
-    for(let c = s; c <= e; c += f) {
-        res.innerHTML += `${c} \u{1F449}` 
+    if (f <= 0) {
+      alert('Factor invalid! Considering factor as 1')
+      f = 1
     }
-    res.innerHTML += `\u{1F3C1}`
+    if (s  < e) {
+      //counting up
+      for(let c = s; c <= e; c += f) {
+          res.innerHTML += `${c} \u{1F449}` 
+      }
+    } else {
+      //counting down
+      for (let c = s; c >= e; c-= f) {
+        res.innerHTML += `${c} \u{1F449}`
+      }
+    }
+     res.innerHTML += `\u{1F3C1}`
   }
 }
