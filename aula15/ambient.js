6afb7f4ed62b2f7a10af2789bb6def579e202764
Jean <jeantoneli@gmail.com>
2025-06-06 23:50:51 +0100

    testing simple arrays

---
+++ aula15/ambient.js
@@ -0,0 +1,7 @@
+let num = [5, 8, 4, 6, 7]
+num.push(1)
+num.sort()
+console.log(num)
+console.log(`the array has ${num.length} positions`)
+console.log(`The value of the first position is ${num[0]}`)
+
