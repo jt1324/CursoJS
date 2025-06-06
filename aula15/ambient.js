2e7d071b362f7752d84e2542e2d41f6390020928
Jean <jeantoneli@gmail.com>
2025-06-06 23:51:47 +0100

    adding comments

--- aula15/ambient.js
+++ aula15/ambient.js
@@ -1,6 +1,6 @@
 let num = [5, 8, 4, 6, 7]
-num.push(1)
-num.sort()
+num.push(1) //add a new value to the array
+num.sort() //sort the array
 console.log(num)
 console.log(`the array has ${num.length} positions`)
 console.log(`The value of the first position is ${num[0]}`)
