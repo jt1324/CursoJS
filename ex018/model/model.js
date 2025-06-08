d987b032e6101c09862a5e287b37251da31cfc72
Jean <jeantoneli@gmail.com>
2025-06-09 00:44:36 +0100

    more js added - second function - finish

--- ex018/model/model.js
+++ ex018/model/model.js
@@ -8,7 +8,19 @@ function add() {
     if (n > 100) {
       alert('Please add a numbe between 1 and 100')
     } else {
-      alert('OK')
+      let item = document.createElement('option')
+      item.text = `Valor ${n} added`
+      res.appendChild(item)
     }
+  }
+}
+
+function fin() {
+  let res = document.getElementById('list')
+  
+  if (res.value.length == 0) {
+    alert('No numbers were added')
+  } else {
+    alert('Done')
   }
 }
