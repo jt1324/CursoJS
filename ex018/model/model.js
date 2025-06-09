b3b9b3f540711090443cb59df812481cfc700a06
Jean <jeantoneli@gmail.com>
2025-06-09 21:40:14 +0100

    complete exercise

--- ex018/model/model.js
+++ ex018/model/model.js
@@ -1,26 +1,60 @@
-function add() {
-  let num = document.getElementById('addtxt')
-  let lis = document.getElementById('list')
-  if (num.value.length == Number(0)) {
-    alert('Please add a number')
-  } else { 
-    let n = Number(num.value)
-    if (n > 100) {
-      alert('Please add a numbe between 1 and 100')
-    } else {
-      let item = document.createElement('option')
-      item.text = `Valor ${n} added`
-      lis.appendChild(item)
-    }
+let num = document.getElementById('addtxt')
+let lis = document.getElementById('list')
+let res = document.querySelector('.res')
+let valu = []
+
+function isNumber(n) {
+  if(Number(n) >= 1 && Number(n) <= 100) {
+    return true
+  } else {
+  return false
   }
 }
 
+function inList(n, l) { 
+  if (l.indexOf(Number(n)) != -1) {
+    return true
+  } else {
+    return false
+  }
+}
+
+function add() {
+  if (isNumber(num.value) && !inList(num.value, valu)) {
+    valu.push(Number(num.value))
+    let item = document.createElement('option')
+    item.text = `Value ${num.value} added`
+    lis.appendChild(item)
+    res.innerHTML = ''
+  } else { 
+    alert('Value invalid or already present in the list.')
+  }
+  num.value = ''
+  num.focus()
+}
+
 function fin() {
-  let lis = document.getElementById('list')
-  let res = document.querySelector('.res')
-  if (lis.value.length == 0) {
+    
+  if (valu.length == 0) {
     alert('No numbers were added')
   } else {
-    res.innerHTML = 'In total tehre are X numbers.</b> The highest number informed was X.</b> The smaller number informed was X.</b> Summing all the values, we have x.</b> The average of the number added is x.'
+    let tot = valu.length
+    let avg = 0
+    let sum = 0
+    let max = valu[0]
+    let min = valu[0]
+    for (let pos in valu) {
+      sum += valu[pos]
+      if (valu[pos] > max)
+        max = valu[pos]
+      if (valu[pos] < min)
+        min = valu[pos]
+    }
+    avg = sum / tot
+    res.innerHTML = ''
+    res.innerHTML += `<p>In total there are ${tot} numbers.</p> <p> The highest number informed was ${max}.</p> <p>The smaller number informed was ${min}.</p> <p>Summing all the values, we have ${sum}.</p> <p>The average of the number added is ${avg}.</p>`
+
+
+ 
   }
 }
